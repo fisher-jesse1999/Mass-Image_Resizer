9b9103714477146618660175620d2ac7c3f7417c
Jesse Fisher <dayguz1999@gmail.com>
2021-07-15 20:11:11 -0400

    improved error checking and added option to create altered copies

--- resizer.py
+++ resizer.py
@@ -6,24 +6,38 @@ import cv2 as cv
 # sys.argv[1] = image file
 # sys.argv[2] = width, in inches
 # sys.argv[3] = height, in inches
+# sys.argv[4] = overwrite existing file? (yes/no)
 def main():
 
     # check argument count
-    if len(sys.argv) != 4:
-        print("invalid number of arguments; argc = ", len(sys.argv))
+    if len(sys.argv) != 5:
+        print("invalid number of arguments; argc = ", len(sys.argv), "/5")
         print("\tsys.argv[0] = script name")
         print("\tsys.argv[1] = image file")
         print("\tsys.argv[2] = width, in inches")
         print("\tsys.argv[3] = height, in inches")
+        print("\tsys.argv[4] = overwrite existing file? (yes/no)")
         exit()
 
     # read in the specified image file
     image = cv.imread(sys.argv[1])
 
+    # make sure that the provided file is a valid image file
     if (not type(image) is numpy.ndarray):
         print("Invalid image file: ", sys.argv[1])
         exit()
 
+    # read in the overwrite argument & format it
+    input = str(sys.argv[4])
+    input = input.lower()
+
+    # set the overwrite flag accordingly
+    if input.startswith("y"):
+        overwrite = True
+    else :
+        overwrite = False
+
+    # attempt to pull the image dimensions from the source
     try:
         # retrieve the original dimensions. the underscore indicates a value were not interested in
         originalWidth, originalHeight =  image.shape
@@ -48,6 +62,11 @@ def main():
         # were shrinking the image
         resizeType = cv.INTER_AREA
 
+    elif originalWidth == width and originalHeight == height:
+        # image is already the desired size
+        print("Image is already the desired size: ", sys.argv[1])
+        exit()
+
     else: 
         # stretching the image in some way
         resizeType = cv.INTER_AREA
@@ -55,8 +74,12 @@ def main():
     # create the resized image
     resized = cv.resize(image, dimension, interpolation=resizeType)
 
-    # overwrite the original image file
-    cv.imwrite(sys.argv[1], resized)
+    if overwrite:
+        # overwrite the original image file
+        cv.imwrite(sys.argv[1], resized)
+    else:
+        # create a new file for the altered version. adding the resized- prefix
+        cv.imwrite(("resized-" + sys.argv[1]), resized)
 
     print("Finished processing: ", sys.argv[1])
 
--- run.bat
+++ run.bat
@@ -1,8 +1,34 @@
 @echo off
+echo ===== Jesse's Image Resizer =====
+echo Warning: resizing the same image multiple times can degrade the quality of the photo.
+echo Consider having the program not overwrite your existing images if you are unsure of the size you want.
+
 set /p width="Please enter the new width (in inches): "
 set /p height="Please enter the new height (in inches): "
+set /p overwrite="Would you like your current images overwritten? (yes/no): "
+
 set startTime=%time%
-forfiles /M *.png /C "cmd /c python resizer.py @FILE %width% %height%"
-forfiles /M *.jpg /C "cmd /c python resizer.py @FILE %width% %height%"
+
+@REM process any png files in the current directory
+if exist *.png (
+    forfiles /M *.png /C "cmd /c python resizer.py @FILE %width% %height% %overwrite%"
+) else (
+    echo No .png files were found, moving on.
+)
+
+@REM process any jpg files in the current directory
+if exist *.jpg (
+    forfiles /M *.jpg /C "cmd /c python resizer.py @FILE %width% %height% %overwrite%"
+) else (
+    echo No .jpg files were found, moving on.
+)
+
+@REM check for resized files, and create a new directory for them if needed
+if exist resized-* (
+    if not exist resizedPhotos mkdir resizedPhotos
+    move resized-* resizedPhotos
+    echo Your resized files have been stored in the resizedPhotos folder.
+)
+
 echo Start: %startTime%
 echo Finished: %time%
